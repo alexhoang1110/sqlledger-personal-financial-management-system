aba8919205c876b579873ffe021afd6fae9bff91
Alex Hoang <alexhoangggg11@gmail.com>
2026-05-14 08:09:36 +0700

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,6 +1,8 @@
 DB_CONFIG = {
-    "host": "localhost",
+    "host": "personal-finance-db-sqlteam6neu.h.aivencloud.com",
+    "port": 24863,
     "user": "finance_app_user",
-    "password": "User1234",
-    "database": "personalfinancedb"
+    "password": "AVNS_6gOttD29pc4MSbLfUro",
+    "database": "PersonalFinanceDB",
+    "ssl_ca": "ca.pem"
 }
