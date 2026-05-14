448cff858f8260fddc2bad08e23db63db00a59c0
Alex Hoang <alexhoangggg11@gmail.com>
2026-05-14 08:09:47 +0700

    Update 07_security.sql

--- sql/07_security.sql
+++ sql/07_security.sql
@@ -1,9 +1,9 @@
 -- User
-CREATE USER IF NOT EXISTS 'finance_app_user'@'localhost' IDENTIFIED BY 'User1234';
-GRANT SELECT, INSERT, UPDATE ON PersonalFinanceDB.* TO 'finance_app_user'@'localhost';
+CREATE USER IF NOT EXISTS 'finance_app_user'@'%' IDENTIFIED BY 'AVNS_6gOttD29pc4MSbLfUro';
+GRANT SELECT, INSERT, UPDATE ON PersonalFinanceDB.* TO 'finance_app_user'@'%';
 
 -- Viewer User
-CREATE USER IF NOT EXISTS 'finance_viewer'@'localhost' IDENTIFIED BY 'View1234';
-GRANT SELECT ON PersonalFinanceDB.* TO 'finance_viewer'@'localhost';
+CREATE USER IF NOT EXISTS 'finance_viewer'@'%' IDENTIFIED BY 'AVNS_EJ7Ou954b5hrphQdnIG';
+GRANT SELECT ON PersonalFinanceDB.* TO 'finance_viewer'@'%';
 
 FLUSH PRIVILEGES;
